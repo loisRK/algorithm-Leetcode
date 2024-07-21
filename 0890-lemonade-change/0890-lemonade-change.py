d489db4f85a1c682c6bfe1e66c126e5134b9e560
RK <39821066+loisRK@users.noreply.github.com>
2024-07-21 19:48:59 +0900

    Time: 648 ms (28.07%), Space: 16.3 MB (16.19%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.py
+++ 0890-lemonade-change/0890-lemonade-change.py
@@ -4,8 +4,8 @@ class Solution(object):
         :type bills: List[int]
         :rtype: bool
         """
-        five = []
-        ten = []
+        five = 0
+        ten = 0
         cnt = 0
 
         if bills[0] > 5:
@@ -13,19 +13,19 @@ class Solution(object):
 
         for i in range(len(bills)):
             if bills[i] == 5:
-                five.append(bills[i])
+                five += 1
             elif bills[i] == 10:
-                if len(five) > 0:
-                    ten.append(bills[i])
-                    del five[0]
+                if five > 0:
+                    ten += 1
+                    five -= 1
                 else:
                     return False
             elif bills[i] == 20:
-                if len(five) > 0 and len(ten) > 0:
-                    del five[0]
-                    del ten[0]
-                elif len(five) >= 3 and len(ten) == 0:
-                    five = five[3:]
+                if five > 0 and ten > 0:
+                    five -= 1
+                    ten -= 1
+                elif five >= 3 and ten == 0:
+                    five -= 3
                 else:
                     return False
 
