6e7c35801052f99352a9e0bbdb1d18ebc1c6e201
RK <39821066+loisRK@users.noreply.github.com>
2024-07-21 19:21:19 +0900

    Time: 110 ms (93.34%), Space: 13.1 MB (20.47%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.py
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -8,19 +8,28 @@ class Solution(object):
         g.sort(reverse=True)
         s.sort(reverse=True)
         cnt = 0
-        chk = 0
-        start = 0
+        gs = 0
+        ss = 0
+    
 
-        while chk!=len(g) and len(s)!=0:
-            i=0
-            for j in range(start, len(g)):
-                if s[i] >= g[j]:
-                    del g[j]
-                    del s[i]
-                    cnt += 1
-                    i += 1
-                    start = j
-                    break
-                chk += 1
+        while gs <= len(g)-1 and ss <= len(s)-1:
+            if s[ss] >= g[gs]:
+                gs += 1
+                ss += 1
+                cnt += 1
+            else:
+                gs += 1
+
+        # while chk!=len(g) and len(s)!=0:
+        #     i=0
+        #     for j in range(start, len(g)):
+        #         if s[i] >= g[j]:
+        #             del g[j]
+        #             del s[i]
+        #             cnt += 1
+        #             i += 1
+        #             start = j
+        #             break
+        #         chk += 1
         
         return cnt
